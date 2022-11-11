b42ed1ec26792505aedca9a9a483caf4cbb7c2f8
Sam Vendittelli <sam.vendittelli@hotmail.com>
2022-11-11 14:06:02 +0000

    ci(e2e): add e2e execution to ci
    Run e2e tests before pushing to GitHub and as part of the CI pipeline.
    
    Note that the record key is only associated with the home-e2e project.
    If we have more projects later a new we will need a new strategy for the
    Cypress dashboard.

--- apps/home-e2e/cypress.config.ts
+++ apps/home-e2e/cypress.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
+  projectId: 'ob94a9',
   e2e: nxE2EPreset(__dirname),
 });
