324f4980593d0fea3901d154a6318d0931cc6703
Sam Vendittelli <sam.vendittelli@hotmail.com>
2022-11-19 14:05:27 +0000

    feat(start-page): add search box
    Add DuckDuckGo search to the start page.

--- apps/start-page/src/app/app.tsx
+++ apps/start-page/src/app/app.tsx
@@ -1,10 +1,12 @@
-import { Box, Text } from 'dracula-ui';
+import { Box } from 'dracula-ui';
 import 'dracula-ui/styles/dracula-ui.css';
+import styles from './app.module.scss';
+import Search from './search/search';
 
 function App() {
   return (
-    <Box>
-      <Text>This is the start page</Text>
+    <Box className={styles['container']} height="full" width="full">
+      <Search />
     </Box>
   );
 }
