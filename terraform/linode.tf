c808a13499004eff542ca6ad17dbfd791b3d82cf
Sam Vendittelli <sam.vendittelli@hotmail.com>
2023-04-14 16:50:14 +0100

    fix(terraform): correct terraform variable

--- terraform/linode.tf
+++ terraform/linode.tf
@@ -2,7 +2,7 @@
 resource "linode_lke_cluster" "k8s" {
   k8s_version = var.k8s_version
   label       = "k8s"
-  region      = var.region
+  region      = var.linode_region
 
   dynamic "pool" {
     for_each = var.pools
